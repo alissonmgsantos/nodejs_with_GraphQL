3f846524077107f223779080393c6140c98fa144
Alisson Matos <alissonmgsantos@gmail.com>
2019-09-27 09:54:07 -0300

    Implementando graphql

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-const customExpress = require('./config/custom-express')
-const conexao = require('./infraestrutura/conexao')
-const Tabelas = require('./infraestrutura/database/tabelas')
-
-const app = customExpress()
+const { GraphQLServer } = require("graphql-yoga");
+const conexao = require("./infraestrutura/conexao");
+const Tabelas = require("./infraestrutura/database/tabelas");
 
 conexao.connect(erro => {
   if (erro) {
-    console.log(erro)
+    console.log(erro);
   }
 
-  console.log('conectou no banco')
+  console.log("conectou no banco");
 
-  Tabelas.init(conexao)
-})
+  Tabelas.init(conexao);
+});
 
-app.listen(4000, () => {
-  console.log('Servidor rodando na porta 4000')
-})
+const resolvers = {
+  Query: {
+    status: () => "Servidor rodando"
+  }
+};
+const server = new GraphQLServer({
+  resolvers: resolvers,
+  typeDefs: "./schema.graphql"
+});
+
+server.start(() => console.log("Servidor ouvindo"));
