a1f25bcea06f9e2bbaaa1d0e0ab67ea36c0e7f31
Alisson Matos <alissonmgsantos@gmail.com>
2019-09-27 10:47:56 -0300

    Finalização do CRUD

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ const resolvers = {
     cliente: (root, { id }) => Clientes.buscaPorId(id)
   },
   Mutation: {
-    adicionarCliente: (root, params) => Clientes.adiciona(params)
+    adicionarCliente: (root, params) => Clientes.adiciona(params),
+    atualizarCliente: (root, params) => Clientes.atualiza(params),
+    deletarCliente: (root, { id }) => Clientes.deleta(id)
   }
 };
 const server = new GraphQLServer({
